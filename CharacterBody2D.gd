c7eb3f0fb3cfc9b39067a0dca29868e2a6969658
konkord16 <52043777+konkord16@users.noreply.github.com>
2024-07-16 22:18:52 +0400

    ?

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -300.0
 
 var jumping = false
 
@@ -21,7 +21,7 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		jumping = true
 
