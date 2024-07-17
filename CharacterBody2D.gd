04222691b00a6e249136e341f1b1844a554425e2
konkord16 <52043777+konkord16@users.noreply.github.com>
2024-07-17 14:31:20 +0400

    cleaning up code
    need to implement animation player for more detailed animation control

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -2,62 +2,45 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
+const GRAVITY = 20
+var grounded : bool = true
+@onready var animation_player = $AnimationPlayer
+@onready var animated_sprite = $Pivot/AnimatedSprite2D
 
-var jumping = false
-
-@onready var animated_sprite = $Node2D/AnimatedSprite2D
-
-@onready var node_2d = $Node2D
-
-
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta):
+func _physics_process(_delta):	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += GRAVITY
 
 	# Handle jump.
 	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		jumping = true
-
+		velocity.y = JUMP_VELOCITY		
+	
+	# Handle moving left/right
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
+		velocity.x = move_toward(velocity.x, 0, SPEED)	
+	
+	# Handle animations
 	if direction == 0:
-			animated_sprite.play("idle")
+		animated_sprite.play("idle")
 	else:
 		animated_sprite.play("run")
-
 		
 	if direction > 0:
-		node_2d.scale.x = 1
-	if direction < 0:                                                        
-		node_2d.scale.x = -1
-		
+		$Pivot.scale.x = 1
+	elif direction < 0:                                                        
+		$Pivot.scale.x = -1		
 		
 	if velocity.y > 0:
 		animated_sprite.play("fall")
 	elif velocity.y < 0:
 		animated_sprite.play("jump")
-		
-	if jumping == true and is_on_floor():
-		animated_sprite.play("ground")
-		jumping = false
-		
-		
 	
+	if is_on_floor() and not grounded:
+		animated_sprite.play("ground")			
+	grounded = is_on_floor()
 	
-	
-	
-		
-
-
 	move_and_slide()
