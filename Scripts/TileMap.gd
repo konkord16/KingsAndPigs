fe76e5e5063974b440567b527a5180e0e2d32a58
konkord16 <52043777+konkord16@users.noreply.github.com>
2024-07-18 19:41:13 +0400

    can now add scenes from tilemap

--- Scripts/TileMap.gd
+++ Scripts/TileMap.gd
@@ -1,7 +1,13 @@
 extends TileMap
 
 @export var TILE_SCENES: Dictionary = {
-	Vector2(1,0): preload("res://Characters/player.tscn")
+	Vector2(0,0): preload("res://Characters/player.tscn"),
+	Vector2(1,0): preload("res://Characters/pig.tscn"),
+	#Vector2(2,0): preload("res://Characters/crate.tscn"),
+	#Vector2(3,0): preload("res://Characters/bomb.tscn"),
+	#Vector2(4,0): preload("res://Characters/crate_pig.tscn"),
+	#Vector2(5,0): preload("res://Characters/cannon.tscn"),
+	#Vector2(6,0): preload("res://Characters/king.tscn"),
 }
 @onready var half_cell_size := tile_set.tile_size * 0.5
 
@@ -9,14 +15,15 @@ var search_layer := 2
 
 func _ready():
 	await get_tree().process_frame
-	#print(get_cell_source_id(2, Vector2(1,0)))
-	#_replace_tiles_with_scene()
+	_replace_tiles_with_scene()
+	
 
 func _replace_tiles_with_scene(scene_dictionary: Dictionary = TILE_SCENES):
 	for key in scene_dictionary.keys():
-		for tile_pos in get_used_cells_by_id(search_layer, 0, key):
+		for tile_pos in get_used_cells_by_id(search_layer, 5, key):
 				var object_scene = scene_dictionary[key]
 				_replace_tile_with_object(tile_pos, object_scene)
+				
 
 func _replace_tile_with_object(tile_pos: Vector2, object_scene: PackedScene, parent: Node = get_tree().current_scene):
 	# Clear the cell in TileMap
@@ -25,7 +32,7 @@ func _replace_tile_with_object(tile_pos: Vector2, object_scene: PackedScene, par
 	
 	#Spawn the object	
 	if object_scene:
-		var obj = object_scene.instantiate() as StaticBody2D
+		var obj = object_scene.instantiate()
 		var ob_pos = map_to_local(tile_pos) - half_cell_size 
 		
 		parent.add_child(obj)
