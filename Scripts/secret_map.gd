fa6289ed7592d347ae41ec68ef97aebe7f143a2f
konkord16 <52043777+konkord16@users.noreply.github.com>
2024-07-24 23:28:00 +0400

    finished second level, added secret areas

---
+++ Scripts/secret_map.gd
@@ -0,0 +1,16 @@
+extends TileMap
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):		
+		print("detecting")
+		var tween = create_tween()
+		tween.tween_property(self, "modulate",Color(1,1,1,0), 0.25)
+
+
+func _on_area_2d_body_exited(body):
+	
+	if body.is_in_group("player"):	
+			
+		print("detecting")	
+		var tween = create_tween()
+		tween.tween_property(self, "modulate",Color(1,1,1,1), 0.25)
